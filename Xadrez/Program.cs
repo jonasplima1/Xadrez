de81a98239cd1d50d1299f4123e687c07f30e270
Jonas Pereira De Lima <lima.1959@teleperformance.com.br>
2021-09-27 17:28:50 -0300

    Captura de peças

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -18,12 +18,7 @@ namespace Xadrez
 
 
                         Console.Clear();
-                        Tela.imprimirTabuleiro(partida.tab);
-                        Console.WriteLine();
-
-                        Console.WriteLine("Turno: " + partida.turno);
-                        Console.WriteLine(" Aguardando jogada: " + partida.jogadorAtual);
-                        Console.WriteLine();
+                        Tela.imprimirPartida(partida);
 
                         Console.Write("Origem: ");
                         Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
