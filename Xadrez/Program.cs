e56120964e0d042fda1c5f09b495bdf9c1ad9616
Jonas Pereira De Lima <lima.1959@teleperformance.com.br>
2021-09-27 15:59:12 -0300

    Implementando movimentação

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -10,15 +10,22 @@ namespace Xadrez
         {
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 4));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                    Console.Write("");
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
+                }
 
 
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
-                Tela.imprimirTabuleiro(tab);
             }
             catch (Exception e)
             {
