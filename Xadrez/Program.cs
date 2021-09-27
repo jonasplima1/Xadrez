e869f5cae5b6a6043d256b142d603a70bd7dc00b
Jonas Pereira De Lima <lima.1959@teleperformance.com.br>
2021-09-27 16:35:14 -0300

    Inserindo movimentos válidos ao Rei e Torre

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -16,9 +16,16 @@ namespace Xadrez
                     Console.Clear();
                     Tela.imprimirTabuleiro(partida.tab);
 
-                    Console.Write("");
+                    Console.WriteLine("");
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                    Console.WriteLine("");
                     Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
