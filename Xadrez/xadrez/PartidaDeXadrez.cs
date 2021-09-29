df110e6a44681d67731687d374a7d8633739a9d6
Jonas Pereira De Lima <lima.1959@teleperformance.com.br>
2021-09-29 12:06:01 -0300

    Jogadda especial promoção

--- Xadrez/xadrez/PartidaDeXadrez.cs
+++ Xadrez/xadrez/PartidaDeXadrez.cs
@@ -147,6 +147,21 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colar em xeque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            //promoção de peão
+            if(p is Peao)
+            {
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -166,7 +181,7 @@ namespace xadrez
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
+            
             //en passant
             if(p is Peao && (destino.linha == origem.linha -2 || destino.linha == origem.linha + 2)){
                 vulneravelEnPassant = p;
